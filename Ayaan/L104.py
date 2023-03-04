fccd4b26b2c92e2e9d6d6bd3568a291bca4994db
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-05 00:48:25 +0900

    Ayaan : 104.Maximum Depth of Binary Tree / Python 2차

--- Ayaan/L104.py
+++ Ayaan/L104.py
@@ -1,23 +1,11 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        return dfs(root)
-
-def dfs(root):
-    if not root:
-        return 0
+        def dfs(root):
+            if not root:
+                return 0
     
-    left_depth = dfs(root.left)
-    right_depth = dfs(root.right)
-
-    return max(left_depth, right_depth) + 1
-
-# class Solution:
-#     def maxDepth(self, root: Optional[TreeNode]) -> int:
-#         def dfs(root):
-#             if not root:
-#                 return 0
-    
-#             left_depth = dfs(root.left)
-#             right_depth = dfs(root.right)
+            left_depth = dfs(root.left)
+            right_depth = dfs(root.right)
             
-#             return max(left_depth, right_depth) + 1
+            return max(left_depth, right_depth) + 1
+        return dfs(root)
