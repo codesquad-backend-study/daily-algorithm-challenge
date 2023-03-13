84acb4b72054530aa4136f707259e4a6ffa3ae46
Sully <2dcoder@naver.com>
2023-03-14 00:46:23 +0900

    Sully: 13. Roman to Integer

---
+++ Sully/L13.py
@@ -0,0 +1,42 @@
+class Solution:
+    # 로마자 예외: 4, 9, 40, 90, 400, 900
+    # ex) IX: 1 + 8 = 9 -> X가 원래 10인데 -2단계가 된 값인 8로 처리가 됨
+    # 위와 같은 규칙 이용
+    def romanToInt(self, s: str) -> int:
+        sum_value = 0
+        before_symbol = ''
+
+        for i in range(len(s)):
+            current_symbol = s[i]
+            # 위에 써놓은 예외 처리 (-2단계)
+            if before_symbol == 'I' and current_symbol == 'V':
+                sum_value += 3
+            elif before_symbol == 'I' and current_symbol == 'X':
+                sum_value += 8
+            elif before_symbol == 'X' and current_symbol == 'L':
+                sum_value += 30
+            elif before_symbol == 'X' and current_symbol == 'C':
+                sum_value += 80
+            elif before_symbol == 'C' and current_symbol == 'D':
+                sum_value += 300
+            elif before_symbol == 'C' and current_symbol == 'M':
+                sum_value += 800
+            # 현재 로마 문자 다 더해주기
+            elif current_symbol == 'I':
+                sum_value += 1
+            elif current_symbol == 'V':
+                sum_value += 5
+            elif current_symbol == 'X':
+                sum_value += 10
+            elif current_symbol == 'L':
+                sum_value += 50
+            elif current_symbol == 'C':
+                sum_value += 100
+            elif current_symbol == 'D':
+                sum_value += 500
+            elif current_symbol == 'M':
+                sum_value += 1000
+            # 현재 문자가 이전 문자가 되고, 다음 반복 때 현재 문자가 갱신됨
+            before_symbol = current_symbol
+
+        return sum_value
