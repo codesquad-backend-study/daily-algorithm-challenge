5096d4b5d85eb5ce697628e51d697f9f422f3a8e
he2joo <hottestej6@gamil.com>
2023-03-21 15:36:36 +0900

    Joy : 통과과

--- Joy/70_ClimbingStairs.py
+++ Joy/70_ClimbingStairs.py
@@ -15,4 +15,17 @@ class Solution:
             way += len(list(combinations(total,one)))
             two += -1
             one += 2
-        return way
+        # return way
+    
+        # 풀이 2. 
+        if n == 1:
+            return 1
+        
+        step = [0] * (n+1)
+        step[1] = 1
+        step[2] = 2
+        
+        for i in range(3, n+1):
+            step[i] = step[i-1] + step[i-2]
+            
+        return step[n]
