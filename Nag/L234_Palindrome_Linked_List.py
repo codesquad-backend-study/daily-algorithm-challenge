64f25207f5183598a896a84bc8f7ac229e62c251
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-22 20:56:45 +0900

    Nag: Palindrome Linked List 주석 추가

--- Nag/L234_Palindrome_Linked_List.py
+++ Nag/L234_Palindrome_Linked_List.py
@@ -11,7 +11,7 @@ class Solution:
         while pointer is not None:
             nodeList.append(pointer.val)
             pointer = pointer.next
-        print(nodeList)
+        #리스트 슬라이싱, 노드가 홀수개 있으면 가장 중앙에 있는 값은 신경 안써도 됩니다(어차피 비교 안해도 되니까)
         before = nodeList[:len(nodeList) // 2 ]
         after = nodeList[len(nodeList) // 2:]
         after.reverse()
