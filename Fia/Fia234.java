7bc66833d26d5395e5f846d7d4081e046d549a90
yeon <cerealnum@gmail.com>
2023-03-22 13:29:11 +0900

    Fia : Palindrome Linked List

---
+++ Fia/Fia234.java
@@ -0,0 +1,23 @@
+package Fia;
+
+public class Fia234 {
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder builder = new StringBuilder();
+        if (head.next == null) { // node가 1개인 경우
+            return true;
+        }
+
+        while (head != null) {
+            builder.append(head.val);
+            head = head.next;
+        }
+
+        String front = builder.substring(0, builder.length() / 2);
+        String back = builder.reverse().substring(0, builder.length() / 2);
+        if (front.equals(back)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
