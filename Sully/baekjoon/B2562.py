be22c1e037a67f426de33e3c73fa891343ce2a2c
Sully <2dcoder@naver.com>
2023-03-27 00:18:12 +0900

    Update B2562.py

--- Sully/baekjoon/B2562.py
+++ Sully/baekjoon/B2562.py
@@ -1,3 +1,15 @@
 import sys
 
-sys.stdin.readline()
+# sys.stdin.readline(): BufferedReader와 동일
+# 근데 이렇게 선언해주면 안 됨.. []가 붙어서 출력되게 됨
+# 이것 땜에 1시간 가까이 날렸는데 June의 도움으로 해결
+# array = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
+
+array = []
+
+for _ in range(9):
+    array.append(int(sys.stdin.readline()))
+
+# 출력
+print(max(array))
+print(array.index(max(array)) + 1)
