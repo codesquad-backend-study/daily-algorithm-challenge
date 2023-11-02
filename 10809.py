47dd0d29ce40629f3583792a191c99705d2c2540
yeonise <cerealnum@gmail.com>
2023-11-02 09:02:52 +0900

    delete file

--- 10809.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-word = sys.stdin.readline()
-
-for i in range(97,123) :
-    char = chr(i)
-    times = word.find(char)
-    print(times, end=" ")
--- 1152.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sentence = list(sys.stdin.readline().split())
-sentence2 = list(map(str,sys.stdin.readline().split()))
-print(len(sentence))
-print(len(sentence2))
-
-print(sentence)
-print(sentence2)
