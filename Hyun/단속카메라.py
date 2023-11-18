e68fede9a575d6c0b25037fc3ff683ae33284c42
Hyun <ghkdgus29@naver.com>
2023-11-18 11:33:09 +0900

    Hyun: 단속카메라 (개선)

--- Hyun/단속카메라.py
+++ Hyun/단속카메라.py
@@ -1,16 +1,13 @@
 def solution(routes):
     routes.sort()
-    start = -30001
     end = 30001
     camera_count = 1
 
     for route in routes:
         if end < route[0]:
             camera_count += 1
-            start = route[0]
             end = route[1]
 
-        start = max(start, route[0])
         end = min(end, route[1])
 
     return camera_count
